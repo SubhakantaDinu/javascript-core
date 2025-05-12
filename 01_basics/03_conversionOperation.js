c583e77cfe1ef25c38ad271a2f13e0baf3026646
Subhakanta Tarai <99885174+SubhakantaDinu@users.noreply.github.com>
2025-05-12 10:34:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score ="33abc"
+
+//console.log(typeof "score");
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" =>NaN  (not an number) but its type is number
+// true =>1; false =>0;
+
+let isLoggedIn = "subhakanta"; 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 =>true; 0 =>false
+//"" => false
+//"hitesh" =>true;
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+
+
