242b4a32bc8143f90d170f6a85fb3ecdcf5812d8
Subhakanta Tarai <99885174+SubhakantaDinu@users.noreply.github.com>
2025-05-13 07:32:07 +0000

    conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,6 +22,49 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************ Operation ************************
+
+let value = 3;
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**2);power 
+// console.log(2/3);power
+// console.log(2%3);
+
+let str1 ="hello"
+let str2 =" subhakanta"
+let str3=str1 + str2;
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2+2);
+// console.log(1 + 2+"2");
+
+// console.log( (3 + 4) * 5 % 3 );
+
+console.log(+true);
+
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2;
+
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);
+
+//link to study
+//https://tc39.es/ecma262/#sec-abstract-operations
+
+
 
 
