dd2625758a7273310bfad9e272caefbf14c232c9
Subhakanta Tarai <99885174+SubhakantaDinu@users.noreply.github.com>
2025-05-13 10:43:23 +0000

    datatypes-sum

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -51,7 +51,7 @@ let str3=str1 + str2;
 
 // console.log( (3 + 4) * 5 % 3 );
 
-console.log(+true);
+// console.log(+true);
 
 console.log(+"");
 
@@ -60,7 +60,7 @@ num1=num2=num3=2+2;
 
 let gamecounter = 100;
 gamecounter++;
-console.log(gamecounter);
+// console.log(gamecounter);
 
 //link to study
 //https://tc39.es/ecma262/#sec-abstract-operations
---
+++ 01_basics/datatpes-summary.js
@@ -0,0 +1,39 @@
+// Primitive
+// 7 tpes : String,Number,Boolean,null,undefined,Symbol and BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let useremail;
+
+const id = Symbol('1234');
+const anotherId =Symbol('1234');
+console.log(id ===anotherId);
+
+// const bigNumber = 12345678888876543n
+
+
+//Reference (Non primitive)
+
+//Array,Objects,Functions
+const heros =["Son","Don","radhe"];
+let myObjt=
+{
+    name:"Radha Kishori",
+    age:25,
+}
+
+const myfunction=function(){
+console.log("Hello Ram");
+}
+
+console.log(typeof myfunction);
+
+
+
+
+// Reference Type (Non primitive)
+
+// Array,Objects,Function
